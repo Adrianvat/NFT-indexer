9fa48d086ead19976d6faa0fbb35de70d5e9aaca
unknown <adrianvat24@icloud.com>
2023-01-18 22:27:27 +0200

    added routes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.post('/crawler', (req, res) => {
+
+})
+
+app.get('/crawler/:jobId', (req, res) => {
+
+})
+
+app.get('contract/:address/:tokenId', (req, res) => {
+    
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
